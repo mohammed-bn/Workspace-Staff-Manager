671628521a59d78993dc580d3f2482396cbdcd90
mohammed-bn <mbentiss11@gmail.com>
2025-11-20 14:11:16 +0100

    ajoute la formulair des experiences chaque fois itylisateur clicker sur button (ajouter un expérience)

--- Workspace-Staff-Manager.js
+++ Workspace-Staff-Manager.js
@@ -17,3 +17,32 @@ function fermme(elementEvent, elementDom) {
 fermme(CloseModal, manageJobModal);
 fermme(butModalFermer, manageJobModal);
 
+
+
+addExperienceButton.addEventListener("click", () => {
+    const nouveauBlocExp = document.createElement("div");
+    nouveauBlocExp.classList.add('exp_global');
+    nouveauBlocExp.innerHTML =`<div class="container_info">
+                                <label for="text"> entreprise : </label><br>
+                                <input class="input_info" type="text" id="expérience">
+                                <span></span>
+                            </div>
+                            <div class="container_info">
+                                <label for="text"> rôle : </label><br>
+                                <input class="input_info" type="text" id="rôle">
+                                <span></span>
+                            </div>
+                            <div class="container_info">
+                                <label for="date">date de début : </label><br>
+                                <input class="input_info" type="date">
+                                <span></span>
+                            </div>
+                            <div class="container_info">
+                                <label for="date"> date de fin : </label><br>
+                                <input class="input_info" type="date">
+                                <span></span>
+                            </div>`
+    document.querySelector(".experiences").appendChild(nouveauBlocExp);
+});
+
+
